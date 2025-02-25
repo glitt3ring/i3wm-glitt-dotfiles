daacc4ccf1251e266f990351eadf4996878e8e44
glitt3ring <glitt3ring356@gmail.com>
2025-02-25 22:50:39 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,8 +11,8 @@ cp -vr .alacritty.toml ~/
 chmod +x ~/.config/polybar/launch.sh
 echo Installing LightDM...
 sudo pacman -Syu --needed --noconfirm lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
-echo Type systemctl enable lightdm and systemctl start lightdm.
-echo You can add exec_always xrandr -s 1920x1080 to your i3wm config.
+echo "Type systemctl enable lightdm and systemctl start lightdm."
+echo "You can add exec_always xrandr -s 1920x1080 to your i3wm config."
 echo Config file is located in "~/.config/i3/config"
 echo Polybar Config is located in "~/.config/polybar/config.ini"
 echo Alacritty Config is located in "~/.alacritty.toml"
